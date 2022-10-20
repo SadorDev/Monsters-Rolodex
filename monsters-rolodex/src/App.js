1bbf38d1f0638c5c78c63fd7e2e125d60d9c5e1c
Sador <Zerezghi@live.co.uk>
2022-10-20 23:55:29 +0100

    Added users from API

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import { Component } from "react";
 
@@ -7,14 +7,31 @@ class App extends Component {
     super();
 
     this.state = {
-      name: 'Sador'
-    }
+      monsters: [],
+    };
+  }
+
+  // The link is a promise which is asynchronous. Essentially a promise is a promise that we're going to return a value.
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users").then((response) =>
+      response.json().then((users) =>
+        this.setState(() => {
+          return { monsters: users };
+        })
+      )
+    );
   }
 
   render() {
     return (
       <div className="App">
-      
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1> {monster.name} </h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
