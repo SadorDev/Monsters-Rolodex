38f20ae69def63009e73728ec40c9b7c1bbd70da
Sador <Zerezghi@live.co.uk>
2022-10-26 02:24:41 +0100

    Created class component for Card-list

---
+++ monsters-rolodex/src/Components/Card-list/card-list-component.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react;";
+
+class CardList extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        <h1> Hello World</h1>
+      </div>
+    );
+  }
+}
+
+export default CardList;
