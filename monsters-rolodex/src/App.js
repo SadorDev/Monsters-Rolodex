c56a6bbc9652aade5578e2add37607d033490071
Sador <Zerezghi@live.co.uk>
2022-10-26 04:57:25 +0100

    Constructed app with CardList compoennt

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,6 +1,6 @@
-// import logo from "./logo.svg";
 import "./App.css";
 import { Component } from "react";
+import CardList from "./Components/Card-list/card-list-component";
 
 class App extends Component {
   constructor() {
@@ -8,6 +8,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
 
@@ -22,19 +23,53 @@ class App extends Component {
     );
   }
 
+  // This was created to optimize performance
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    // This sets monster as the value, and is it going to recieve a callback
+    // and going to pass and call the callback on each element in the array
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
   render() {
+    // These were created for optimization - we removed this and state from the two variables
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((el) =>
+      el.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
+        <input
+          className="search"
+          type="search"
+          placeholder="Search Monsters"
+          // This is an anonymous function
+          onChange={onSearchChange}
+        />
+
+        {/* We don't need this as since we created the Card List Component.  */}
+
+        {/* {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
-              <h1> {monster.name} </h1>
+              <h1> {monster.name}</h1>
             </div>
           );
-        })}
+        })} */}
+
+        {/* This component shouldn't have to think about 
+        filtering its monster list. It only cares about what it's 
+        going to display. So we use the "monster" prop so it can 
+        render it to the UI*/}
+
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
 }
-
 export default App;
